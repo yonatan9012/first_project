bbd0baebccaad44452b2e32aa8eb1f89404c8060
User <ovadiayonatan0@gmail.com>
2025-11-10 17:42:02 +0200

    with swager

--- src/main/java/com/first/project/ProjectApplication.java
+++ src/main/java/com/first/project/ProjectApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class ProjectApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ProjectApplication.class, args);
+        SpringApplication.run(ProjectApplication.class, args);
 	}
 
 }
